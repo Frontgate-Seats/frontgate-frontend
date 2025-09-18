9a9cd0b2c562a6c413a6326b9ec3701276e5db36
mayank-frontgate <mayank@frontgateseats.com>
2025-09-18 14:09:29 +0000

    Add dummy events data and represent in dashboard

--- src/pages/dashboard.page.tsx
+++ src/pages/dashboard.page.tsx
@@ -1,5 +1,244 @@
-const DashboardPage = () => {
-  return <>{"Dashboard"}</>;
-};
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+import {
+  DataGrid,
+  GridActionsCellItem,
+  type GridColDef,
+  type GridFilterModel,
+  type GridPaginationModel,
+  type GridSortModel,
+  type GridEventListener,
+  gridClasses,
+} from '@mui/x-data-grid';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import { useLocation, useNavigate, useSearchParams } from 'react-router';
 
-export default DashboardPage;
+import {
+  getManyEvents as getEvents,
+  type Event,
+} from '../data/events';
+import PageContainer from './PageContainer';
+import { Typography } from '@mui/material';
+
+const INITIAL_PAGE_SIZE = 10;
+
+export default function DashboardPage() {
+  const { pathname } = useLocation();
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+
+  const [paginationModel, setPaginationModel] = React.useState<GridPaginationModel>({
+    page: searchParams.get('page') ? Number(searchParams.get('page')) : 0,
+    pageSize: searchParams.get('pageSize')
+      ? Number(searchParams.get('pageSize'))
+      : INITIAL_PAGE_SIZE,
+  });
+  const [filterModel, setFilterModel] = React.useState<GridFilterModel>(
+    searchParams.get('filter')
+      ? JSON.parse(searchParams.get('filter') ?? '')
+      : { items: [] },
+  );
+  const [sortModel, setSortModel] = React.useState<GridSortModel>(
+    searchParams.get('sort') ? JSON.parse(searchParams.get('sort') ?? '') : [],
+  );
+
+  const [rowsState, setRowsState] = React.useState<{
+    rows: Event[];
+    rowCount: number;
+  }>({
+    rows: [],
+    rowCount: 0,
+  });
+  console.log("rowsState : ", rowsState)
+
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [error, setError] = React.useState<Error | null>(null);
+
+  const handlePaginationModelChange = React.useCallback(
+    (model: GridPaginationModel) => {
+      setPaginationModel(model);
+
+      searchParams.set('page', String(model.page));
+      searchParams.set('pageSize', String(model.pageSize));
+
+      const newSearchParamsString = searchParams.toString();
+
+      navigate(
+        `${pathname}${newSearchParamsString ? '?' : ''}${newSearchParamsString}`,
+      );
+    },
+    [navigate, pathname, searchParams],
+  );
+
+  const handleFilterModelChange = React.useCallback(
+    (model: GridFilterModel) => {
+      setFilterModel(model);
+
+      if (
+        model.items.length > 0 ||
+        (model.quickFilterValues && model.quickFilterValues.length > 0)
+      ) {
+        searchParams.set('filter', JSON.stringify(model));
+      } else {
+        searchParams.delete('filter');
+      }
+
+      const newSearchParamsString = searchParams.toString();
+
+      navigate(
+        `${pathname}${newSearchParamsString ? '?' : ''}${newSearchParamsString}`,
+      );
+    },
+    [navigate, pathname, searchParams],
+  );
+
+  const handleSortModelChange = React.useCallback(
+    (model: GridSortModel) => {
+      setSortModel(model);
+
+      if (model.length > 0) {
+        searchParams.set('sort', JSON.stringify(model));
+      } else {
+        searchParams.delete('sort');
+      }
+
+      const newSearchParamsString = searchParams.toString();
+
+      navigate(
+        `${pathname}${newSearchParamsString ? '?' : ''}${newSearchParamsString}`,
+      );
+    },
+    [navigate, pathname, searchParams],
+  );
+
+  const loadData = React.useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+
+    try {
+      const listData = await getEvents({
+        paginationModel,
+        sortModel,
+        filterModel,
+      });
+
+      setRowsState({
+        rows: listData.items.filter(Boolean),
+        rowCount: listData.itemCount,
+      });
+    } catch (listDataError) {
+      setError(listDataError as Error);
+    }
+
+    setIsLoading(false);
+  }, [paginationModel, sortModel, filterModel]);
+
+  React.useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  const handleRefresh = React.useCallback(() => {
+    if (!isLoading) {
+      loadData();
+    }
+  }, [isLoading, loadData]);
+
+  const handleRowClick = React.useCallback<GridEventListener<'rowClick'>>(
+    ({ row }) => {
+    },
+    [navigate],
+  );
+
+
+  const initialState = React.useMemo(
+    () => ({
+      pagination: { paginationModel: { pageSize: INITIAL_PAGE_SIZE } },
+    }),
+    [],
+  );
+
+  const columns: GridColDef<Event>[] = [
+    { field: 'id', headerName: 'ID' },
+    { field: 'eventName', headerName: 'Event Name', width: 180 },
+    { field: 'performerName', headerName: 'Performer', width: 160 },
+    {
+      field: 'eventDate',
+      headerName: 'Date',
+      type: 'date',
+      width: 120,
+      valueGetter: (eventDate: string) => eventDate ? new Date(eventDate) : null,
+    },
+    { field: 'eventTime', headerName: 'Time', width: 100 },
+    { field: 'section', headerName: 'Section', width: 80 },
+    { field: 'row', headerName: 'Row', width: 80 },
+    { field: 'price', headerName: 'Price', type: 'number', align: 'left', headerAlign: 'left', width: 100, valueGetter: (price: string) => `$${price}` },
+    {
+      field: 'actions',
+      type: 'actions',
+      getActions: (params: any) => [
+        <Button variant='contained' color='primary' sx={{
+          margin: 1
+        }}>
+          <Typography>Buy Now</Typography>
+        </Button>,
+      ],
+    },
+  ];
+
+  const pageTitle = 'Events';
+
+  return (
+    <PageContainer
+      title={pageTitle}
+      breadcrumbs={[{ title: pageTitle }]}
+      actions={
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Tooltip title="Reload data" placement="right" enterDelay={1000}>
+            <div>
+              <IconButton size="small" aria-label="refresh" onClick={handleRefresh}>
+                <RefreshIcon />
+              </IconButton>
+            </div>
+          </Tooltip>
+        </Stack>
+      }
+    >
+      <Box sx={{ flex: 1, width: '100%' }}>
+        {error ? (
+          <Box sx={{ flexGrow: 1 }}>
+            <Alert severity="error">{error.message}</Alert>
+          </Box>
+        ) : (
+          <DataGrid<Event>
+            rows={rowsState.rows}
+            rowCount={rowsState.rowCount}
+            columns={columns}
+            pagination
+            sortingMode="server"
+            filterMode="server"
+            paginationMode="server"
+            paginationModel={paginationModel}
+            onPaginationModelChange={handlePaginationModelChange}
+            sortModel={sortModel}
+            onSortModelChange={handleSortModelChange}
+            filterModel={filterModel}
+            onFilterModelChange={handleFilterModelChange}
+            disableRowSelectionOnClick
+            onRowClick={handleRowClick}
+            loading={isLoading}
+            initialState={initialState}
+            showToolbar
+            pageSizeOptions={[5, INITIAL_PAGE_SIZE, 25]}
+          />
+        )}
+      </Box>
+    </PageContainer>
+  );
+}
